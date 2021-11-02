60671351a0622b008f77a83318ebd4b16e48300b
Jake Hiney <hiney.jake@outlook.com>
2021-11-02 15:45:05 -0500

    Make fib prettier

--- fib.js
+++ fib.js
@@ -1,10 +1,10 @@
 fib = (n) => {
-	if (n == 0) return n;
-	else if (n == 1) return n;
+	if(n < 2) return n;
 	else return fib(n - 1) + fib(n - 2);
 };
 
 printFib = (n) => {
+    //fib always starts with 0
 	console.log(0);
 	if (n > 1) {
 		for (let i = 1; i <= n; i++) {
@@ -13,4 +13,4 @@ printFib = (n) => {
 	}
 };
 
-printFib(30);
+printFib(4);

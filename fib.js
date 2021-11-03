892dd69ce1c89d9f50e7c4039e379855b7079a96
Jake Hiney <hiney.jake@outlook.com>
2021-11-03 08:21:33 -0500

    feat: show amount of times fib has run.

--- fib.js
+++ fib.js
@@ -1,16 +1,22 @@
+var i = 0;
+
 fib = (n) => {
-	if(n < 2) return n;
-	else return fib(n - 1) + fib(n - 2);
+	if (n < 2) return n;
+	else {
+		i++;
+		return fib(n - 1) + fib(n - 2);
+	}
 };
 
 printFib = (n) => {
-    //fib always starts with 0
+	//fib always starts with 0
 	console.log(0);
 	if (n > 1) {
 		for (let i = 1; i <= n; i++) {
 			console.log(fib(i));
 		}
 	}
+	console.log(`fib ran: ${i} times!`);
 };
 
-printFib(4);
+printFib(40);
